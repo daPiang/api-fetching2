2cd83045507c35cbf295594ced7df6387f74a59e
Whytel Macro <daPiang@mcm.edu.ph>
2022-01-31 03:39:52 +0800

    added marker arrays

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,13 +1,16 @@
 import * as L from '../../lib/leaflet/leaflet-src.esm.js';
 
 //Map Stuff
-const map = L.map('birdMap').setView([14.599512,120.984222],5);
+const map = L.map('birdMap').setView([14.599512,120.984222],7);
 const attribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
 const mapTiles = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
 const tiles = L.tileLayer(mapTiles, {attribution});
 tiles.addTo(map);
 
-let api_link_main = 'https://api.ebird.org/v2/data/obs/PH/recent';
+let markers = [];
+
+//API Stuff
+let api_link_main = 'https://api.ebird.org/v2/data/obs/PH/recent?back=30';
 let api_link_reg = 'https://api.ebird.org/v2/ref/region/info/PH';
 
 let myHeaders = new Headers();
@@ -43,17 +46,29 @@ fetch(api_link_main, requestOptions)
         response.json()
             .then(function(json) {
                 console.log(json); //Our Actual Data
-                console.log(json[0]); //Test Data
+                // console.log(json[0]); //Test Data
 
-                let marker = L.marker([json[99].lat,json[99].lng]).addTo(map); //Bird Marker
-                marker.bindPopup(
-                    '<center>'+json[99].comName+'</center>'+
-                    '<br><center>'+json[99].sciName+'</center>');
+                //TODO: Fix Same Marker Location
+                for (let i = 0; i < 10; i++) {
+                    markers.push([
+                        json[i].lat,
+                        json[i].lng,
+                        '<center>'+json[i].comName+'</center><br><center>'+json[i].sciName+'</center>',
+                        json[i].locName
+                    ]);
+                    
+                    let lat = markers[i][0];
+                    let lng = markers[i][1];
+                    let popupText = markers[i][2];
 
-                let marker2 = L.marker([json[120].lat,json[120].lng]).addTo(map); //Bird Marker
-                marker2.bindPopup(
-                    '<center>'+json[120].comName+'</center>'+
-                    '<br><center>'+json[120].sciName+'</center>');
+                    let marker_loc = new L.LatLng(lat, lng);
+                    let marker = new L.Marker(marker_loc);
+                    map.addLayer(marker);
+
+                    marker.bindPopup(popupText);
+                    console.log('Loop '+(i+1));
+                    console.log(markers);
+                }
             })
     })
     .catch(function(error) {
